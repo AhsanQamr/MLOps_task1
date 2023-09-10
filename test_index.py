afa6ab8b5d5874efe2ee692cc5deb1c3dc08226e
HRH@DESKTOP-0JP7NF9 <qaahsan92@gmail.com>
2023-09-10 09:43:53 +0500

    first commit

---
+++ test_index.py
@@ -0,0 +1,18 @@
+import unittest
+from index import add_numbers
+
+class TestAddNumbers(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        result = add_numbers(2, 3)
+        self.assertEqual(result, 5)
+
+    def test_add_negative_numbers(self):
+        result = add_numbers(-2, -3)
+        self.assertEqual(result, -5)
+
+    def test_add_mixed_numbers(self):
+        result = add_numbers(5, -3)
+        self.assertEqual(result, 2)
+
+if __name__ == '__main__':
+    unittest.main()
